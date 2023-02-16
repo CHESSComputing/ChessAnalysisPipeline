2fe49b1acc287925fb06a4f8f2a62b2dbae7421e
Keara Soloway <kls286@cornell.edu>
2023-02-16 08:59:44 -0500

    add: implementation of reader.MultipleReader.
    This Reader enables Processors to use input data from multiple files.

--- reader.py
+++ reader.py
@@ -5,6 +5,7 @@ Description: generic Reader module
 """
 
 # system modules
+import sys
 import yaml
 
 # local modules
@@ -36,6 +37,20 @@ class Reader():
             data = file.read()
         return data
 
+class MultipleReader(Reader):
+    def read(self, readers):
+        '''Return resuts from multiple `Reader`s.
+
+        :param readers: a list where each item is a tuple representing (in order)
+            the name of the reader, the specific type of the `Reader`, and a
+            dictionary of arguments to pass as keywords to that `Reader`'s `read`
+            method (usually: `{"filename": "<filename>"}`).
+        :type readers: list[tuple[str,Reader,dict[str,str]]]
+        :return: The results of calling `Reader.read(**kwargs)` for each item in `readers`.
+        :rtype: list[tuple(str,object)]
+        '''
+        data = [(r[0], getattr(sys.modules[__name__],r[1])().read(**r[2])) for r in readers]
+        return(data)
 
 class YAMLReader(Reader):
     def read(self, filename):
