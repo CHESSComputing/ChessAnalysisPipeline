f910145f1bc2fa2d19e8ac1ca4f01b99b7e176a8
Valentin Kuznetsov <vkuznet@gmail.com>
2023-03-16 12:00:46 -0400

    First implementation of TFaaSImageProcessor

--- processor.py
+++ processor.py
@@ -53,6 +53,52 @@ class Processor():
         # and we return data back to pipeline
         return data
 
+
+class TFaaSImageProcessor(Processor):
+    '''
+    A Processor to get predictions from TFaaS inference server.
+    '''
+    def process(self, data, url, model, verbose=False):
+        """
+        process data API
+        """
+
+        t0 = time()
+        self.logger.info(f'Executing "process" with url {url} model {model}')
+
+        data = self._process(data, url, model, verbose)
+
+        self.logger.info(f'Finished "process" in {time()-t0:.3f} seconds\n')
+
+        return(data)
+
+    def _process(self, data, url, model, verbose):
+        '''Print and return the input data.
+
+        :param data: Input image data, either file name or actual image data
+        :type data: object
+        :return: `data`
+        :rtype: object
+        '''
+        from MLaaS.tfaas_client import predictImage
+        from pathlib import Path
+        print(f'{self.__name__} data :')
+        if isinstance(data, str) and Path(data).is_file():
+            imgFile = data
+            data = predictImage(url, imgFile, model, verbose)
+        else:
+            rdict = data[0]
+            import requests
+            img = rdict['data']
+            session = requests.Session()
+            req = session.post(url + '/predict/image', files=dict(image=img), data=dict(model=model) )
+            data = req.content
+            data = data.decode("utf-8").replace('\n', '')
+            print("TFaas response", data)
+
+        return(data)
+
+
 class PrintProcessor(Processor):
     '''A Processor to simply print the input data to stdout and return the
     original input data, unchanged in any way.
