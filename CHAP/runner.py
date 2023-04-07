dbaac7ea8088996487e5a02cf568575358f5baee
Valentin Kuznetsov <vkuznet@gmail.com>
2023-04-07 09:21:08 -0400

    Re-factor the code to make run/setLogger functions

--- CHAP/runner.py
+++ CHAP/runner.py
@@ -40,18 +40,35 @@ def runner(opts):
     :param opts: opts is an instance of argparse.Namespace which contains all input parameters
     """
 
-    logger = logging.getLogger(__name__)
-    log_level = getattr(logging, opts.log_level.upper())
-    logger.setLevel(log_level)
-    log_handler = logging.StreamHandler()
-    log_handler.setFormatter(logging.Formatter('{name:20}: {message}', style='{'))
-    logger.addHandler(log_handler)
-
+    log_level = opts.log_level.upper()
+    logger, log_handler = setLogger(log_level)
     config = {}
     with open(opts.config) as file:
         config = yaml.safe_load(file)
     logger.info(f'Input configuration: {config}\n')
     pipeline_config = config.get('pipeline', [])
+    run(pipeline_config, logger, log_level, log_handler)
+
+def setLogger(log_level="INFO"):
+    """
+    Helper function to set CHAP logger
+
+    :param log_level: logger level, default INFO
+    """
+    logger = logging.getLogger(__name__)
+    log_level = getattr(logging, log_level.upper())
+    logger.setLevel(log_level)
+    log_handler = logging.StreamHandler()
+    log_handler.setFormatter(logging.Formatter('{name:20}: {message}', style='{'))
+    logger.addHandler(log_handler)
+    return logger, log_handler
+
+def run(pipeline_config, logger=None, log_level=None, log_handler=None):
+    """
+    Run given pipeline_config
+
+    :param pipeline_config: CHAP pipeline config
+    """
     objects = []
     kwds = []
     for item in pipeline_config:
@@ -65,16 +82,22 @@ def runner(opts):
         modName, clsName = name.split('.')
         module = __import__(f'CHAP.{modName}', fromlist=[clsName])
         obj = getattr(module, clsName)()
-        obj.logger.setLevel(log_level)
-        obj.logger.addHandler(log_handler)
-        logger.info(f'Loaded {obj}')
+        if log_level:
+            obj.logger.setLevel(log_level)
+        if log_handler:
+            obj.logger.addHandler(log_handler)
+        if logger:
+            logger.info(f'Loaded {obj}')
         objects.append(obj)
         kwds.append(kwargs)
     pipeline = Pipeline(objects, kwds)
-    pipeline.logger.setLevel(log_level)
-    pipeline.logger.addHandler(log_handler)
-    logger.info(f'Loaded {pipeline} with {len(objects)} items\n')
-    logger.info(f'Calling "execute" on {pipeline}')
+    if log_level:
+        pipeline.logger.setLevel(log_level)
+    if log_handler:
+        pipeline.logger.addHandler(log_handler)
+    if logger:
+        logger.info(f'Loaded {pipeline} with {len(objects)} items\n')
+        logger.info(f'Calling "execute" on {pipeline}')
     pipeline.execute()
 
 
