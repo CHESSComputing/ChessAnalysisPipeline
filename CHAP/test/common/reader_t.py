c87a1c84476e2503a6ab28312236e86076d177ee
Keara Soloway <kls286@cornell.edu>
2023-05-09 08:45:34 -0400

    add: remaining unittests for Readers defined in CHAP.common

--- CHAP/test/common/reader_t.py
+++ CHAP/test/common/reader_t.py
@@ -1,33 +1,89 @@
+#!/usr/bin/env python
 """
-File       : common/reader.py
+File       : common/reader_t.py
 Author     : Valentin Kuznetsov <vkuznet AT gmail dot com>
 Description: Unit tests for common/reader.py code
 """
 
 # system modules
 import time
+import os
 import unittest
 
 # local modules
-from CHAP.common import YAMLReader
+from CHAP.common import (BinaryFileReader,
+                         NexusReader,
+                         URLReader,
+                         YAMLReader)
 
 
-class CommonReaderTest(unittest.TestCase):
-    """Unit test for common/reader.py module"""
+class BinaryFileReaderTest(unittest.TestCase):
+    """Unit test for CHAP.common.BinaryFileReader class"""
 
     def setUp(self):
-        self.fname = '/'.join(__file__.split("/")[:-2]) + '/data/file.yaml'
-        self.reader = YAMLReader()
-
-    def tierDown(self):
-        pass
+        self.reader = BinaryFileReader()
+        self.filename = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            'data/img.png')
 
     def testReader(self):
+        """Unit test to test reader"""
+        data = self.reader.read(self.filename)
+        self.assertIsInstance(data, bytes)
+
+
+class NexusReaderTest(unittest.TestCase):
+    """Unit test for CHAP.common.BinaryFileReader class"""
+
+    def setUp(self):
+        self.reader = NexusReader()
+        self.filename = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            'data/file.nxs')
+        self.nxpath = 'entry/data'
+
+    def testReader(self):
+        """Unit test to test reader"""
+        from nexusformat.nexus import NXroot
+        data = self.reader.read(self.filename)
+        self.assertIsInstance(data, NXroot)
+
+    def testNXpath(self):
+        """Unit test to test the `nxpath` keyword argument of
+        `NexusReader.read`
         """
-        Unit test to test reader
-        """
-        data = self.reader.read(self.fname)
-        self.assertEqual(isinstance(data, dict), True)
+        from nexusformat.nexus import NXdata
+        data = self.reader.read(self.filename, nxpath=self.nxpath)
+        self.assertIsInstance(data, NXdata)
+
+
+class URLReaderTest(unittest.TestCase):
+    """Unit test for CHAP.common.URLReader class"""
+
+    def setUp(self):
+        self.reader = URLReader()
+        self.url = 'tbd'
+
+    def testReader(self):
+        """Unit test to test reader"""
+        pass
+        # data = self.reader.read(self.url)
+        # self.assertIsInstance(data, bytes)
+
+
+class YAMLReaderTest(unittest.TestCase):
+    """Unit test for CHAP.common.YAMLReader class"""
+
+    def setUp(self):
+        self.reader = YAMLReader()
+        self.filename = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            'data/file.yaml')
+
+    def testReader(self):
+        """Unit test to test reader"""
+        data = self.reader.read(self.filename)
+        self.assertIsInstance(data, dict)
 
 
 if __name__ == '__main__':
