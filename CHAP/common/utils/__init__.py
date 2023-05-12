4dc2d3712ac964b0494326bb4520563a944d3525
Keara Soloway <kls286@cornell.edu>
2023-05-12 11:54:12 -0400

    doc: add package docstrings.

---
+++ CHAP/common/utils/__init__.py
@@ -0,0 +1,4 @@
+"""This subpackage contains generic utilities for fitting, parsing
+CHESS scan data, collecting interactive user input, and finding
+lattice properties of materials (among others).
+"""
--- CHAP/tomo/__init__.py
+++ CHAP/tomo/__init__.py
@@ -1,2 +1,6 @@
+"""This subpackage contains `PipelineItems` unique to tomography data
+processing workflows.
+"""
+
 from CHAP.common import MapProcessor
 from CHAP.tomo.processor import TomoDataProcessor
