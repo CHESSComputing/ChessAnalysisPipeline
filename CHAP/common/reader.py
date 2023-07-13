b7d89244401db6d54f5bd9d251ff432079254ff3
Keara Soloway <kls286@cornell.edu>
2023-07-13 07:26:28 -0400

    add: initial implementation of CHAP.common.MapReader

--- CHAP/common/reader.py
+++ CHAP/common/reader.py
@@ -29,6 +29,97 @@ class BinaryFileReader(Reader):
         return data
 
 
+class MapReader(Reader):
+    """Reader for CHESS sample maps"""
+    def read(self, map_config):
+        """Take a map configuration dictionary and return a
+        representation of the map as an NXentry. The NXentry's default
+        data group will contain the raw data collected over the course
+        of the map.
+
+        :param map_config: map configurationto be passed directly to
+            the constructor of `CHAP.common.models.map.MapConfig`
+        :type map_config: dict
+        :return: Data from the map configuration provided
+        :rtype: nexusformat.nexus.NXentry
+        """
+        from json import dumps
+        from nexusformat.nexus import (NXcollection,
+                                       NXdata,
+                                       NXentry,
+                                       NXfield,
+                                       NXsample)
+        import numpy as np
+        from CHAP.common.models.map import MapConfig
+
+        # Validate the map configuration provided by constructing a
+        # MapConfig
+        map_config = MapConfig(**map_config)
+
+        # Set up NXentry and add misc. CHESS-specific metadata
+        nxentry = NXentry(name=map_config.title)
+        nxentry.map_config = dumps(map_config.dict())
+
+        nxentry.attrs['station'] = map_config.station
+        nxentry.spec_scans = NXcollection()
+        for scans in map_config.spec_scans:
+            nxentry.spec_scans[scans.scanparsers[0].scan_name] = \
+                NXfield(value=scans.scan_numbers,
+                        dtype='int8',
+                        attrs={'spec_file': str(scans.spec_file)})
+
+        # Add sample metadata
+        nxentry[map_config.sample.name] = NXsample(**map_config.sample.dict())
+
+        # Set up default data group
+        nxentry.data = NXdata()
+        nxentry.data.attrs['axes'] = map_config.dims
+        for i, dim in enumerate(map_config.independent_dimensions[::-1]):
+            nxentry.data[dim.label] = NXfield(
+                value=map_config.coords[dim.label],
+                units=dim.units,
+                attrs={'long_name': f'{dim.label} ({dim.units})',
+                       'data_type': dim.data_type,
+                       'local_name': dim.name})
+            nxentry.data.attrs[f'{dim.label}_indices'] = i
+
+        # Set up empty NXfields for scalar data present in the map
+        # configuration provided
+        signal = False
+        auxilliary_signals = []
+        for data in map_config.all_scalar_data:
+            nxentry.data[data.label] = NXfield(
+                value=np.empty(map_config.shape),
+                units=data.units,
+                attrs={'long_name': f'{data.label} ({data.units})',
+                       'data_type': data.data_type,
+                       'local_name': data.name})
+            if not signal:
+                signal = data.label
+            else:
+                auxilliary_signals.append(data.label)
+        if signal:
+            nxentry.data.attrs['signal'] = signal
+            nxentry.data.attrs['auxilliary_signals'] = auxilliary_signals
+
+        # Fill in the empty NXfields with maps of raw scan data
+        for scans in map_config.spec_scans:
+            for scan_number in scans.scan_numbers:
+                scanparser = scans.get_scanparser(scan_number)
+                for scan_step_index in range(scanparser.spec_scan_npts):
+                    map_index = scans.get_index(
+                        scan_number,
+                        scan_step_index,
+                        map_config)
+                    for data in map_config.all_scalar_data:
+                        nxentry.data[data.label][map_index] = data.get_value(
+                            scans,
+                            scan_number,
+                            scan_step_index)
+
+        return nxentry
+
+
 class NexusReader(Reader):
     """Reader for NeXus files"""
     def read(self, filename, nxpath='/'):
