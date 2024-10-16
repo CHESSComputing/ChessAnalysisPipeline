53717d6d77d11a7e73c1c6d88ecf9964cbc5cdf6
Rolf Verberg <rv43@cornell.edu>
2024-10-16 15:13:59 -0400

    add: CHAP.common.processor.UpdateNXvalueProcessor added
    Also fixed CHAP.edd.reader.UpdateNXdataReader to use the
    new UpdateNXvalueProcessor and thus return a list of points
    with nxpath, index and value.

--- CHAP/common/__init__.py
+++ CHAP/common/__init__.py
@@ -24,6 +24,7 @@ from CHAP.common.processor import (
     PyfaiAzimuthalIntegrationProcessor,
     RawDetectorDataMapProcessor,
     SetupNXdataProcessor,
+    UpdateNXvalueProcessor,
     UpdateNXdataProcessor,
     NXdataToDataPointsProcessor,
     XarrayToNexusProcessor,
