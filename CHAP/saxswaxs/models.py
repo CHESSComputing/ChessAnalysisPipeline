bf8dc01343a4c11251918d205c68a2f626698c9b
Keara Soloway <kls286@cornell.edu>
2025-02-27 11:35:49 -0500

    feat: extend  metadata included in PyfaiIntegrationProcessorConfig.zarr_tree

--- CHAP/saxswaxs/models.py
+++ CHAP/saxswaxs/models.py
@@ -7,6 +7,7 @@ from pydantic import (
     model_validator,
     validator,
     conlist,
+    PrivateAttr,
 )
 from typing import Literal, Optional
 from typing_extensions import Annotated
@@ -51,6 +52,7 @@ class PyfaiIntegrationConfig(MyBaseModel):
     integration_method: Literal[
         'integrate1d', 'integrate2d', 'integrate_radial']
     integration_params: dict
+    _placeholder_result: PrivateAttr = None
 
     def integrate(self, ais, input_data):
         from copy import deepcopy
@@ -78,12 +80,7 @@ class PyfaiIntegrationConfig(MyBaseModel):
         if self.integration_method == 'integrate_radial':
             raise NotImplementedError
         else:
-            from pyFAI.multi_geometry import MultiGeometry
-            self.multi_geometry['ais'] = [
-                ais[name]
-                for name in self.multi_geometry['ais']
-            ]
-            mg = MultiGeometry(**self.multi_geometry)
+            mg = self.get_multi_geometry(ais)
             integration_method = getattr(mg, self.integration_method)
             integration_params = {k: v
                                   for k, v in self.integration_params.items()
@@ -119,44 +116,130 @@ class PyfaiIntegrationConfig(MyBaseModel):
                 )
             return results
 
-    @property
-    def axes(self):
+    def init_placeholder_results(self, azimuthal_integrators):
+        placeholder_result = None
+        placeholder_data = self.get_placeholder_data(azimuthal_integrators)
+        if self.integration_method == 'integrate_radial':
+            raise NotImplementedError
+        else:
+            mg = self.get_multi_geometry(azimuthal_integrators)
+            integration_method = getattr(mg, self.integration_method)
+            integration_params = {k: v
+                                  for k, v in self.integration_params.items()
+                                  if k != 'lst_data'}
+            placeholder_result = integration_method(
+                lst_data=placeholder_data, **integration_params)
+        self._placeholder_result = placeholder_result
+
+    def get_multi_geometry(self, azimuthal_integrators):
+        from copy import deepcopy
+        from pyFAI.multi_geometry import MultiGeometry
+
+        ais = [azimuthal_integrators[name]
+               for name in self.multi_geometry['ais']]
+        kwargs = deepcopy(self.multi_geometry)
+        del kwargs['ais']
+        return MultiGeometry(ais, **kwargs)
+
+    def get_placeholder_data(self, azimuthal_integrators):
+        import numpy as np
+
+        data = []
+        for ai in azimuthal_integrators.values():
+            data.append(np.full(ai.detector.shape, 0))
+        return data
+
+    def result_axes(self):
         return []
+
     @property
     def result_shape(self):
-        if self.integration_method == 'integrate1d':
+        if self.integration_method == 'integrate_radial':
             return (self.integration_params['npt'], )
-        if self.integration_method == 'integrate2d':
+        elif self.integration_method == 'integrate1d':
+            return (self.integration_params['npt'], )
+        elif self.integration_method == 'integrate2d':
             return (self.integration_params['npt_azim'],
                     self.integration_params['npt_rad'])
         else:
             raise NotImplementedError(
                 f'Unimplemented integration_method: {self.integration_method}')
-        
+
     @property
-    def result_coords_fields(self):
-        raise NotImplementedError
-        # if self.integration_method == 'integrate1d':
-        #     return {
-        #         'radial': {
-        #             'attributes': {
-        #             }
-        #         }
-        #     }
-        
+    def result_coords(self):
+        import pyFAI.containers
+        import pyFAI.units
+
+        coords = {}
+        if self._placeholder_result is None:
+            raise RuntimeError
+        elif isinstance(self._placeholder_result,
+                        pyFAI.containers.Integrate2dResult):
+            radial_unit = pyFAI.units.to_unit(
+                self._placeholder_result.radial_unit,
+                type_=pyFAI.units.RADIAL_UNITS)
+            coords[radial_unit.name] = {
+                'attributes': {
+                    'units': radial_unit.unit_symbol,
+                    'long_name': radial_unit.label,
+                },
+                'data': self._placeholder_result.radial.tolist(),
+                'shape': self._placeholder_result.radial.shape,
+                'dtype': 'float32',
+            }
+            azimuthal_unit = pyFAI.units.to_unit(
+                self._placeholder_result.azimuthal_unit,
+                type_=pyFAI.units.AZIMUTHAL_UNITS)
+            coords[azimuthal_unit.name] = {
+                'attributes': {
+                    'units': azimuthal_unit.name.split('_')[-1],
+		    'long_name': azimuthal_unit.label,
+                },
+                'data': self._placeholder_result.azimuthal.tolist(),
+                'shape': self._placeholder_result.azimuthal.shape,
+                'dtype': 'float32',
+            }
+        elif isinstance(self._placeholder_result,
+                        pyFAI.containers.Integrate1dResult):
+            if self.integration_method == 'integrate_radial':
+                raise NotImplementedError
+            elif self.integration_method == 'integrate1d':
+                radial_unit = pyFAI.units.to_unit(
+                    self.multi_geometry.get('unit', '2th_deg'),
+                    type_=pyFAI.units.RADIAL_UNITS)
+                coords[radial_unit.name] = {
+                    'attributes': {
+                        'units': radial_unit.unit_symbol,
+                        'long_name': radial_unit.label,
+                    },
+                    'data': self._placeholder_result.radial.tolist(),
+                    'shape': self._placeholder_result.radial.shape,
+                    'dtype': 'float32',
+                }
+            else:
+                raise ValueError
+        else:
+            raise TypeError
+        return coords
+
+    def get_axes_indices(self, dataset_ndims):
+        return {k: dataset_ndims + i
+                for i, k in enumerate(self.result_coords.keys())}
+
     def zarr_tree(self, dataset_shape, dataset_chunks):
         import json
         tree = {
             # NXprocess
             'attributes': {
                 'default': 'data',
-#                'config': json.dumps(self.dict())
+                # 'config': json.dumps(self.dict())
             },
             'children': {
                 'data': {
                     # NXdata
                     'attributes': {
-#                        'axes': []
+                        # 'axes': self.result_axes(),
+                        **self.get_axes_indices(len(dataset_shape))
                     },
                     'children': {
                         'I': {
@@ -168,8 +251,9 @@ class PyfaiIntegrationConfig(MyBaseModel):
                             'dtype': 'int64',
                             'shape': (*dataset_shape, *self.result_shape),
                             'chunks': (*dataset_chunks, *self.result_shape),
+                            'compressors': None,
                         },
-#                        **self.result_coords_fields,
+                        **self.result_coords,
                     }   
                 }
             }
@@ -183,6 +267,9 @@ class PyfaiIntegrationProcessorConfig(MyBaseModel):
     integrations: conlist(min_length=1, item_type=PyfaiIntegrationConfig)
 
     def zarr_tree(self, dataset_shape, dataset_chunks):
+        ais = {ai.name: ai.ai for ai in self.azimuthal_integrators}
+        for integration in self.integrations:
+            integration.init_placeholder_results(ais)
         tree = {
             'root': {
                 'attributes': {
@@ -218,11 +305,9 @@ class PyfaiIntegrationProcessorConfig(MyBaseModel):
                             }
                         }
                     },
+                    **{integration.name: integration.zarr_tree(
+                        dataset_shape, dataset_chunks)},
                 }
             }
         }
-        for integration in self.integrations:
-            tree['root']['children'][integration.name] = integration.zarr_tree(
-                dataset_shape, dataset_chunks)
-        
         return tree
