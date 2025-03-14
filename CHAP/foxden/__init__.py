3a8198dc2d93754f96d57b80ff6fd133f669b194
Valentin Kuznetsov <vkuznet@gmail.com>
2025-03-14 10:32:39 -0400

    correct imports

--- CHAP/foxden/__init__.py
+++ CHAP/foxden/__init__.py
@@ -1,7 +1,6 @@
 """This subpackage contains pieces for communication with FOXDEN services.
 """
 
-from CHAP.foxden.processor import FoxdenProvenanceProcessor, \
-    FoxdenMetaDataProcessor
+from CHAP.foxden.processor import FoxdenProvenanceProcessor, FoxdenMetaDataProcessor
 from CHAP.foxden.reader import FoxdenReader
 from CHAP.foxden.writer import FoxdenWriter
