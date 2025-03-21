5358e1e9f0f80c6ef4610a490034ddd306eb20f7
Valentin Kuznetsov <vkuznet@gmail.com>
2025-03-21 08:52:39 -0400

    Move common utility functions into its own module

--- CHAP/foxden/processor.py
+++ CHAP/foxden/processor.py
@@ -9,15 +9,10 @@ Description: Processor module for FOXDEN services
 
 # System modules
 from time import time
-import os
-import sys
-import platform
-import pkg_resources
-import subprocess
-import json
 
 # Local modules
 from CHAP.processor import Processor
+from CHAP.common import osinfo, environments
 
 class FoxdenMetaDataProcessor(Processor):
     """A Processor to communicate with FOXDEN MetaData server."""
@@ -83,82 +78,6 @@ class FoxdenProvenanceProcessor(Processor):
                 output.append(rec)
         self.logger.info(f'Finished "process" in {time()-t0:.3f} seconds\n')
         return output
-
-def osinfo():
-    """
-    Helper function to provide osinfo
-    """
-    os_info = {
-        "name": platform.system().lower() + "-" + platform.release(),
-        "kernel": platform.version(),
-        "version": platform.platform()
-    }
-    return os_info
-
-def environments():
-    """
-    Detects the current Python environment (system, virtualenv, or Conda) and
-    collects package information. Returns a list of detected environments with
-    installed packages.
-    """
-    environments = []
-    os_name = platform.system().lower() + "-" + platform.release()
-
-    # Check for Conda environment
-    conda_env = os.getenv("CONDA_PREFIX")
-    if conda_env:
-        conda_env_name = os.getenv("CONDA_DEFAULT_ENV", "unknown-conda-env")
-        try:
-            # Fetch Conda packages
-            conda_packages = subprocess.check_output(["conda", "list", "--json"], text=True)
-            conda_packages = json.loads(conda_packages)
-            packages = [{"name": pkg["name"], "version": pkg["version"]} for pkg in conda_packages]
-        except Exception:
-            packages = []
-
-        environments.append({
-            "name": conda_env_name,
-            "version": sys.version.split()[0],
-            "details": "Conda environment",
-            "parent_environment": None,
-            "os_name": os_name,
-            "packages": packages
-        })
-
-    # Check for Virtualenv (excluding Conda)
-    elif hasattr(sys, 'real_prefix') or os.getenv("VIRTUAL_ENV"):
-        venv_name = os.path.basename(os.getenv("VIRTUAL_ENV", "unknown-venv"))
-        packages = [
-            {"name": pkg.key, "version": pkg.version}
-            for pkg in pkg_resources.working_set
-        ]
-
-        environments.append({
-            "name": venv_name,
-            "version": sys.version.split()[0],
-            "details": "Virtualenv environment",
-            "parent_environment": None,
-            "os_name": os_name,
-            "packages": packages
-        })
-
-    # System Python (not inside Conda or Virtualenv)
-    else:
-        packages = [
-            {"name": pkg.key, "version": pkg.version}
-            for pkg in pkg_resources.working_set
-        ]
-
-        environments.append({
-            "name": "system-python",
-            "version": sys.version.split()[0],
-            "details": "System-wide Python",
-            "parent_environment": None,
-            "os_name": os_name,
-            "packages": packages
-        })
-
-    return environments
 
 def inputFiles():
     """
