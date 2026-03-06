6b6949b0f91a0fef0c8f05b456600a5c4823c28f
Rolf Verberg <rv43@cornell.edu>
2026-03-06 15:26:57 -0500

    refactor: implemented GIWAXS wedge correction with pyFAI

--- CHAP/common/map_utils.py
+++ CHAP/common/map_utils.py
@@ -15,7 +15,7 @@ from CHAP.common.models.map import (
 )
 
 def get_axes(nxdata, skip_axes=None):
-    """Get the axes of an NXdata object used in EDD."""
+    """Get the axes of an NXdata object."""
     if skip_axes is None:
         skip_axes = []
     if 'unstructured_axes' in nxdata.attrs:
