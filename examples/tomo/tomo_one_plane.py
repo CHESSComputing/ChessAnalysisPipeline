e8ff4e3958c48b476aeb052b9fcea17b875cb773
Rolf Verberg <rv43@cornell.edu>
2026-04-30 11:52:31 -0400

    test: Updated tomo_one_plane.py to use the hollow_cube example

--- examples/tomo/tomo_one_plane.py
+++ examples/tomo/tomo_one_plane.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""Choose the path to your CHESS repo."""
 import sys
 #sys.path.append(
 #    '/home/rv43/Documents/Programs/repos/CHESSComputing/ChessAnalysisPipeline')
 sys.path.append(
-    '/nfs/chess/sw/CHESS-software-releases/repos/prod/ChessAnalysisPipeline')
+    '/home/rv43/Documents/Programs/repos/CHESSComputing/ChessAnalysisPipeline_main')
+#sys.path.append(
+#    '/nfs/chess/sw/CHESS-software-releases/repos/prod/ChessAnalysisPipeline')
+
+# System modules
+import os
+from pprint import pprint
+from tempfile import NamedTemporaryFile
 
 # Third party modules
 import matplotlib.pyplot as plt
@@ -29,74 +37,80 @@ from CHAP.utils.general import quick_imshow
 # Start user input
 #------------------------------------------------------------------------------#
 
+"""Select run_type from
+'normal':     Full regular reconstruction
+              (like running the default hollow_cube in CHAP)
+'one_plane':  Reconstruction of two planes only
+'two_planes': Reconstruction of one plane only
+'roi':        Reconstruction for a subset or rows
+              (find center at first and last row)
+"""
+#run_type = 'normal'
+#run_type = 'one_plane'
+#run_type = 'two_planes'
+run_type = 'roi'
+
 interactive = True
+outputdir = 'output'
 construct_chess_map = True
-map_name = 'output/map.nxs'
-chess_map_name = 'output/chess_map.nxs'
-load_converted_map = True
-img_row_bounds = [600, 606] # None, [80, 2080] or [1050, 1251]
-recon_layer_index = [600, 1400] # One or two values
-                                # ignored when using img_row_bounds
 reduce_data = True
-reduced_data_name = 'output/reduced_data.nxs'
-reduce_config = {
-    'remove_stripe': {'remove_all_stripe': {}},
-    'img_row_bounds': img_row_bounds,
-}
 find_center = True
-find_center_name = 'output/find_center.yaml'
-find_center_config = {
-    'gaussian_sigma': 0.75,
-#    'remove_stripe_sigma': None,
-    'ring_width': 5,
-}
 reconstruct_data = True
-reconstructed_data_name = 'output/reconstructed_data.nxs'
+
+map_name = f'{outputdir}/map.nxs'
+chess_map_name = f'{outputdir}/chess_map.nxs'
+reduced_data_name = f'{outputdir}/reduced_data.nxs'
+reduce_config = {
+#    'remove_stripe': {'remove_all_stripe': {}},
+}
+find_center_name = f'{outputdir}/find_center.yaml'
+find_center_config = {
+    'gaussian_sigma': 0.05,
+#    'remove_stripe_sigma': None,
+    'ring_width': 1,
+}
+reconstructed_data_name = f'{outputdir}/reconstructed_data.nxs'
 reconstruct_config = {
-    'x_bounds': [90, 2470],
-    'y_bounds': [600, 2000],
-    'secondary_iters:': 25,
-    'gaussian_sigma': 0.75,
-    'remove_stripe_sigma': None,
-    'ring_width': 5,
+    'x_bounds': [15, 390],
+    'y_bounds': [25, 380],
+    'secondary_iters': 10,
+#    'gaussian_sigma': 0.75,
+#    'remove_stripe_sigma': None,
+    'ring_width': 1,
 }
 tdf_scan_numbers = 1
 tbf_scan_numbers = 2
 map_config = {
-    'title': 'badran-4510-b',
+    'title': 'hollow_cube',
     'station': 'id3b',
     'experiment_type': 'TOMO',
-    'sample': {'name': 'sample_190-1-Edge'},
+    'sample': {'name': 'hollow_cube'},
     'spec_scans': [
-        {'spec_file': 'data/sample_190-1-Edge/refine_1',
+        {'spec_file': 'raw/hollow_cube/hollow_cube',
          'scan_numbers': 3}
     ],
     'independent_dimensions': [
         {'label': 'rotation_angles',
          'units': 'degrees',
          'data_type': 'scan_column',
-         'name': 'GI_samphi'},
+         'name': 'theta'},
         {'label': 'x_translation',
          'units': 'mm',
          'data_type': 'spec_motor',
-         'name': 'saxx'},
+         'name': 'GI_samx'},
         {'label': 'z_translation',
          'units': 'mm',
          'data_type': 'spec_motor',
-         'name': 'saxz'}
+         'name': 'GI_samz'}
     ],
-    'presample_intensity': {
-        'data_type': 'scan_column',
-        'name': 'ic1'
-    }
 }
-detector_config = {'detectors': [{'id': 'andor2'}],}
+detector_config = {'detectors': [{'id': 'sim'}],}
 detector_setup = {
-    'prefix': 'andor2',
-    'rows': 2160,
-    'columns': 2560,
-    'pixel_size': [0.0065, 0.0065],
-    'lens_magnification': 5.0,
+    'prefix': 'sim',
+    'rows': 40,
+    'columns': 400,
+    'pixel_size': [0.05, 0.005],
+    'lens_magnification': 1.0,
 }
 remove_stripe_corrections = [
 #    'remove_all_stripe',
@@ -112,33 +126,85 @@ remove_stripe_corrections = [
 ]
 
 #------------------------------------------------------------------------------#
+# Optional user input
+#------------------------------------------------------------------------------#
+
+if run_type == 'normal':
+    img_row_bounds = [3, 35]
+    recon_layer_indices = [11, 28]
+elif run_type == 'one_plane':
+    img_row_bounds = None
+    recon_layer_indices = 15
+elif run_type == 'two_planes':
+    img_row_bounds = None
+    recon_layer_indices = [11, 20]
+elif run_type == 'roi':
+    img_row_bounds = None
+    recon_layer_indices = [11, 20]
+else:
+    print('Pick valid values for img_row_bounds and recon_layer_indices')
+    img_row_bounds = None       # list[int, int], optional
+    recon_layer_indices = None  # int | list[int, int], optional
+                                # ignored when using img_row_bounds
+
+#------------------------------------------------------------------------------#
 # End user input
 #------------------------------------------------------------------------------#
 
+if not os.path.isdir(outputdir):
+    os.makedirs(outputdir)
+try:
+    NamedTemporaryFile(dir=outputdir)
+except Exception as exc:
+    raise OSError(
+        'Output directory not accessible for writing ({outputdir})') from exc
+
+if isinstance(recon_layer_indices, int):
+    recon_layer_indices = [recon_layer_indices]
 if img_row_bounds is None:
-    if len(recon_layer_index)== 1:
+    center_rows = None
+    if len(recon_layer_indices)== 1:
         detector_config['roi'] = [
-            {'start': recon_layer_index,
-             'end': recon_layer_index+1,
+            {'start': recon_layer_indices[0],
+             'end': recon_layer_indices[0]+1,
              'step': None},
             None]
-    elif len(recon_layer_index) == 2:
+    elif len(recon_layer_indices) == 2:
         detector_config['roi'] = [
-            {'start': recon_layer_index[0],
-             'end': recon_layer_index[1],
-             'step': recon_layer_index[1]-recon_layer_index[0]-1},
+            {'start': recon_layer_indices[0],
+             'end': recon_layer_indices[1]},
             None]
+        if run_type == 'two_planes':
+            detector_config['roi'][0]['step'] = \
+                recon_layer_indices[1]-recon_layer_indices[0]-1
+        else:
+            detector_config['roi'][0]['step'] = None
     else:
-        print('Invalid value for recon_layer_index ({recon_layer_index})')
-        exit()
+        raise RuntimeError('Invalid value for recon_layer_indices ({recon_layer_indices})')
 else:
     assert len(img_row_bounds) == 2
-    recon_layer_index = []
-    detector_config['roi'] = [
-        {'start': img_row_bounds[0],
-         'end': img_row_bounds[1],
-         'step': 1},
-        None]
+    center_rows = recon_layer_indices
+    recon_layer_indices = []
+
+reduce_config['img_row_bounds'] = img_row_bounds
+find_center_config['center_rows'] = center_rows
+
+#------------------------------------------------------------------------------#
+
+if construct_chess_map:
+    print(f'\nmap_config:')
+    pprint(map_config)
+if reduce_data:
+    print(f'\nreduce_config:')
+    pprint(reduce_config)
+if find_center:
+    print(f'\nfind_center_config:')
+    pprint(find_center_config)
+if reconstruct_data:
+    print(f'\nreconstruct_config:')
+    pprint(reconstruct_config)
+print(f'\ndetector_config:')
+pprint(detector_config)
 
 # Construct the CHESS style tomo map
 if not construct_chess_map:
@@ -197,9 +263,7 @@ else:
 if not reduce_data:
     nxroot = nxload(reduced_data_name)
 else:
-    # Load as needed
-    if load_converted_map:
-        nxroot = nxload(chess_map_name)
+    nxroot = nxload(chess_map_name)
     # Reduce the data with remove_all_stripe
     data = [
         PipelineData(name='TomoCHESSMapConverter', data=nxroot, schema=None)]
@@ -222,7 +286,7 @@ else:
     image_slice = nxdata.nxsignal[0,:,0,:]
     vmin = image_slice.min()
     vmax = image_slice.max()
-    if len(recon_layer_index):
+    if len(recon_layer_indices):
         quick_imshow(
             image_slice,
             title=f'Slice {detector_config["roi"][0]["start"]}',
@@ -231,7 +295,7 @@ else:
             vmin=vmin, vmax=vmax,
             save_fig=True,
             block=True)
-    if len(recon_layer_index) == 2:
+    if len(recon_layer_indices) == 2:
         quick_imshow(
             image_slice,
             title=f'Slice {detector_config["roi"][0]["end"]}',
@@ -300,6 +364,8 @@ else:
 #            name='TomoFindCenterProcessor', data=center_config,
 #            schema='tomo.models.TomoFindCenterConfig')
     ]
+    from pprint import pprint
+    pprint(reconstruct_config)
     tomo = TomoReconstructProcessor(
         config=reconstruct_config,
         center_config=center_config,
