478bf85915184829697e4cb0a0f8c6850c79eed9
Keara Soloway <kls286@cornell.edu>
2026-06-04 08:31:36 -0400

    fix: local module paths in edd.reader

--- CHAP/edd/reader.py
+++ CHAP/edd/reader.py
@@ -785,7 +785,7 @@ class SliceNXdataReader(Reader):
         from nexusformat.nexus import NXentry, NXfield
 
         # Local modules
-        from CHAP.common import NexusReader
+        from CHAP.common.reader import NexusReader
         from CHAP.utils.general import nxcopy
 
         # Read NXroot
@@ -1015,7 +1015,7 @@ class NXdataSliceReader(Reader):
         from nexusformat.nexus import nxload
 
         # Local modules
-        from CHAP.common import NXdataReader
+        from CHAP.common.reader import NXdataReader
         from CHAP.utils.parfile import ParFile
 
         # Parse existing NXdata
