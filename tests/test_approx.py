83161ee5c93c2b8190f2b446f7c98cef868be74b
Rolf Verberg <rv43@cornell.edu>
2026-08-07 17:13:46 -0400

    test: add tests for the base fit models of utils.fit.FitProcessor

--- tests/test_approx.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-def test_sum():
-    assert (0.1 + 0.2) == 0.3
-
-def test_sum_approx():
-    assert (0.1 + 0.2) == pytest.approx(0.3)
--- tests/test_calculator2.py
+++
@@ -1,6 +0,0 @@
-class TestAdvancedMath:
-    def test_power(self):
-        assert 2 ** 3 == 8
-
-    def test_square_root(self):
-        assert 4 ** 0.5 == 2
